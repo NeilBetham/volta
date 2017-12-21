6c90c011f22027e774a1d3f4b028014b1161f739
Neil Betham <nbetham@me.com>
2017-12-20 23:32:27 -0500

    Initial Commit

---
+++ Volta/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Volta
+//
+//  Created by Neil Betham on 12/20/17.
+//  Copyright © 2017 Neil Betham. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
